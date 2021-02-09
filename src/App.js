63ad3183fde0216d5d3cc84ec00480e99eb3e670
MiyaPeynova <maria.peynova@hotmail.com>
2021-02-09 17:47:24 +0000

    Added link to github repo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Miya's Waether App</h1>
+      <footer> <a href="https://github.com/miya89/react-weather-app" target="_blank">Open-source code</a> by Miya Peynova</footer>
     </div>
   );
 }
